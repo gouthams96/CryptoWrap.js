876fa513c05901bece8b75f171c419b831a8cf4f
gouthams96 <gouthamsajeev1996@gmail.com>
2024-01-03 19:02:19 +0530

    refact(CryptoWrap)

--- src/CryptoWrap.js
+++ src/CryptoWrap.js
@@ -1,41 +1,59 @@
+
 class CryptoWrap {
     constructor() {
+        this.defaultOptions = {
+            name: "RSA-OAEP", // Algorithm
+            modulusLength: 2048, // The length in bits of the RSA modulus.
+            publicExponent: new Uint8Array([0x01, 0x00, 0x01]), // 65537 
+            extractable: true, // Indicating whether it will be possible to export the key using
+            hash: "SHA-256", // Name of the digest function to use.
+        };
         this.keyPair;
     }
-    _arrayBufferToBase64(buffer) {
-        const binary = new Uint8Array(buffer);
-        return btoa(String.fromCharCode(...binary));
-    }
-    _base64ToArrayBuffer(base64) {
-        const binary_string = atob(base64);
-        const len = binary_string.length;
-        const bytes = new Uint8Array(len);
-        for (let i = 0; i < len; i++) {
-            bytes[i] = binary_string.charCodeAt(i);
-        }
-        return bytes.buffer;
-    }
-
-    async exportPublicKey() {
-        const exportedPublicKey = await window.crypto.subtle.exportKey('spki', this.keyPair.publicKey);
-        return this._arrayBufferToBase64(exportedPublicKey);
-    }
     async generateKeyPair() {
-        this.keyPair = await window.crypto.subtle.generateKey(
-            {
-                name: 'RSA-OAEP',
-                modulusLength: 2048,
-                publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-                hash: 'SHA-256',
-            },
-            true,
-            ['encrypt', 'decrypt']
-        );
+        const algorithm = this.defaultOptions;
+        const extractable = true;
+        const keyUsages = ["encrypt", "decrypt"];
+        this.keyPair = await window.crypto.subtle.generateKey(algorithm, extractable, keyUsages);
+    }
+    async getPublicKey() {
+        const format = 'spki';
+        const key = this.keyPair.publicKey;
+        const publicKey = await window.crypto.subtle.exportKey(format, key);
+        return this.arrayBufferToBase64(publicKey);
     }
 
-    // Convert PEM to Uint8Array
-    _pemToUint8Array(pem) {
-        const base64Data = pem.replace(/-----BEGIN PUBLIC KEY-----|-----END PUBLIC KEY-----/g, '');
-        return this._base64ToArrayBuffer(base64Data);
+    async getPrivateKey() {
+        const format = 'pkcs8';
+        const key = this.keyPair.privateKey;
+        const privateKey = await window.crypto.subtle.exportKey(format, key);
+        return this.arrayBufferToBase64(privateKey);
+    }
+
+    async getPublicKeyPem() {
+        const base64Key = await this.getPublicKey();
+        return this.base64ToPem(base64Key, 'PUBLIC KEY');
+    }
+
+    async getPrivateKeyPem() {
+        const base64Key = await this.getPrivateKey();
+        return this.base64ToPem(base64Key, 'PRIVATE KEY');
+    }
+
+    async base64ToPem(base64Key, label) {
+        const pemHeader = `-----BEGIN ${label}-----\n`;
+        const pemFooter = `\n-----END ${label}-----`;
+        const base64KeyWithLineBreaks = base64Key.match(/.{1,64}/g).join('\n');
+        return pemHeader + base64KeyWithLineBreaks + pemFooter;
+    }
+
+    async arrayBufferToBase64(buffer) {
+        const byteArray = new Uint8Array(buffer);
+        let byteString = '';
+        for (let i = 0; i < byteArray.byteLength; i++) {
+            byteString += String.fromCharCode(byteArray[i]);
+        }
+        const base64Key = window.btoa(byteString);
+        return base64Key;
     }
 }
